38737ec305ac3217b82ada3e46cb51dfe1a77bab
Suresh Devalapalli <sureshd@gaussiansolutions.com>
2024-01-04 13:11:21 -0800

    changed absolute path to relative path.

--- src/example.py
+++ src/example.py
@@ -6,7 +6,7 @@ from python_confidence_metric.area_analyzer import AreaAnalyzer
 if __name__ == '__main__':
     osm_data_handler = OSMDataHandler(username='sujatam@gaussiansolutions.com', password='R@lling#1')
     area_analyzer = AreaAnalyzer(osm_data_handler=osm_data_handler)
-    score = area_analyzer.calculate_area_confidence_score(file_path='/Users/anujkumar/Work/Gaussian/TDEI-python-lib-confidence-metric/src/assets/caphill_mini.geojson')
+    score = area_analyzer.calculate_area_confidence_score(file_path='./src/assets/caphill_mini.geojson')
     print(score)
 # 0.5089541985791985
 # 0.5626176462426462
